4d9e0692a0b0169a5e1703ec465d3417c41a65ec
Oliver N <oliverng@gmail.com>
2015-05-30 14:45:32 +0800

    Initial Commit

---
+++ CertificatePinning/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CertificatePinning
+//
+//  Created by Oliver and Vienne Ng on 30/5/15.
+//  Copyright (c) 2015 ONG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CertificatePinning/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CertificatePinning
+//
+//  Created by Oliver and Vienne Ng on 30/5/15.
+//  Copyright (c) 2015 ONG. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
